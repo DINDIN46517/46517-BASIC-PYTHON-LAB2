29aff0688e2ec312e26ec4e82b05a68bb335ebe7
DINDIN46517 <phsuthanssiriphrleis@gmail.com>
2025-09-06 09:44:34 +0700

    hi

--- function/wrok3.py
+++ function/wrok3.py
@@ -1,7 +1,17 @@
 def show_table(n, limit):
-    i = 1   # 
+    i = 1
     while i <= limit:
         print(n, "x", i, "=", n * i)
         i += 1
 
-show_table(5, 12)  
+while True:
+    n = int(input("Enter n: "))
+    if n > 0:
+        break
+
+while True:
+    limit = int(input("Enter limit: "))
+    if limit > 0:
+        break
+
+show_table(n, limit)
